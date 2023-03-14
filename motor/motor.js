4c36106e47b4f683881c74bfac946a0848a31277
Erick-Devi <erickgdevi@gmail.com>
2023-03-14 13:20:37 -0300

    todas paginas de progrma

---
+++ motor/motor.js
@@ -0,0 +1,16 @@
+function calcular()
+{
+
+    
+    var n1 = parseFloat(document.getElementById('nota1').value);
+
+    var n2 = parseFloat(document.getElementById('nota2').value);
+
+    var n3 = parseFloat(document.getElementById('nota3').value);
+  
+       var media = ((n1 + n2 + n3) /3);
+
+        alert(media);
+
+
+}
